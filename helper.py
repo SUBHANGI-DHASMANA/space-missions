d61f47a684e97a3e05c2aeacd0d9eaae92845123
Subhangi Dhasmana <91680044+SUBHANGI-DHASMANA@users.noreply.github.com>
2023-09-29 17:05:21 +0530

    Completed project

--- helper.py
+++ helper.py
@@ -1,10 +1,47 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 import streamlit as st
 import plotly.express as px
-def Company_mission(df):
-    temp = df["Company Name"].value_counts().reset_index()[:28]
-    return temp
+import streamlit as st
+
+
+def Countries_mission(df):
+    df['Country'] = df['Location'].str.split(',').str[-1].str.strip()
+
+    counts = df['Country'].value_counts()
+    countries = counts.index.tolist()
+
+    plt.figure(figsize=(8, 8))
+    ax = sns.barplot(x=counts, y=countries, palette="viridis")
+    ax.set_title("Space Missions of Each Country")
+    ax.set_xlabel("Count")
+    ax.set_ylabel("Country")
+
+    st.pyplot(fig=plt.gcf())
+
+def Company_names(df):
+    company_list = df['Company Name'].unique().tolist()
+    company_list.remove("Arm??e de l'Air")
+    return company_list
+
+def Countries_names(df):
+    df['Country'] = df['Location'].str.split(',').str[-1].str.strip()
+    country_list = df['Country'].unique().tolist()
+    return country_list
+
+def companyCountries(df, country):
+    country_df = df[df['Country'] == country]
+    country_companies = country_df['Company Name'].unique()
+    country_counts = country_df['Company Name'].value_counts()
+    
+    plt.figure(figsize=(8, 8))
+    ax = sns.barplot(x=country_counts, y=country_companies, palette="viridis")
+    ax.set_title(f"Space Missions by Companies in the {country}")
+    ax.set_xlabel("Count")
+    ax.set_ylabel("Company Name")
+    st.pyplot(fig=plt.gcf())
+
 def MissionStatus(df):
     temp = df["Status Mission"].value_counts().reset_index()
     temp.columns = ["Status Mission", "Frequency"]
@@ -17,10 +54,13 @@ def MSChart(temp, df):
 def RocketStatus(df):
     Rocket_status = df["Status Rocket"].value_counts()
     return Rocket_status
-
 def Money(df):
-    df_money = df.groupby(["Company Name"])[" Rocket"].sum().reset_index()
+    df[' Rocket'] = df[' Rocket'].str.replace(',', '', regex=True)
+    df[' Rocket'] = pd.to_numeric(df[' Rocket'], errors='coerce')
+
+    df_money = df.groupby("Company Name")[" Rocket"].sum().reset_index()
     return df_money
+
 def RocketMoney(df):
     df_money = df.groupby(["Company Name"])[" Rocket"].sum().reset_index()
     df_money_TOP5 = df_money.sort_values(by=[" Rocket"], ascending=False).reset_index()[:5]
--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.sidebar.title("Space Mission Analysis")
 st.sidebar.image('./space-mission-img.jpg')
 user_menu = st.sidebar.radio(
     'Select an Option',
-    ('Overall','Company-wise Analysis','Status of Mission', 'Rocket Analysis')
+    ('Overall','Country-wise Analysis','Status of Mission', 'Rocket Analysis')
 )
 
 if user_menu=="Overall":
@@ -24,23 +24,12 @@ if user_menu=="Overall":
     st.title("Description")
     st.write(df.describe())
 
-if user_menu=="Company-wise Analysis":
-    st.title("Companies and their space mission")
-    temp = helper.Company_mission(df)
-    st.dataframe(temp)
-    list28 = np.random.randint(1, 100, size=28)
-    company_names = ["Company 1", "Company 2", "Company 3", "Company 4", "Company 5",
-                     "Company 6", "Company 7", "Company 8", "Company 9", "Company 10",
-                     "Company 11", "Company 12", "Company 13", "Company 14", "Company 15",
-                     "Company 16", "Company 17", "Company 18", "Company 19", "Company 20",
-                     "Company 21", "Company 22", "Company 23", "Company 24", "Company 25",
-                     "Company 26", "Company 27", "Company 28"]
-    fig = plt.figure(figsize=(10, 6))
-    plt.bar(list28, company_names)
-    st.title("Analysis")
-    fig_plotly = go.Figure(data=[go.Bar(x=list28, y=company_names)])
-    st.write("Here is the graphical view of space mission by companies-")
-    st.plotly_chart(fig_plotly)
+if user_menu=="Country-wise Analysis":
+    st.title("Countries and their space mission")
+    helper.Countries_mission(df)
+    st.title("Show analysis with respect to country")
+    selected_company = st.selectbox("Select country", helper.Countries_names(df))
+    helper.companyCountries(df, selected_company)
 
 if user_menu=="Status of Mission":
     st.title("Status of Mission")
@@ -51,7 +40,7 @@ if user_menu=="Status of Mission":
     ax = sns.barplot(data=SM, x=SM['Status Mission'], y=SM.Frequency, errwidth=1, palette="Paired")
     for p in ax.patches:
         ax.annotate('{:.1f}'.format(p.get_height()), (p.get_x() + 0.1, p.get_height() + 1))
-    st.write("Frequencies of Mission Status")
+    st.title("Frequencies of Mission Status")
     st.pyplot(fig)
 
 
@@ -64,9 +53,13 @@ if user_menu=="Status of Mission":
     st.title("Status of Rocket")
     st.pyplot(fig)
 
-if user_menu=="Rocket Analysis":
+if user_menu == "Rocket Analysis":
     st.title("Company spend money")
     df_ = preprocessing.RA(df)
-    st.dataframe(helper.Money(df))
-    st.write("Here are top 5 rocket companies-")
+    money_by_company = helper.Money(df)
+
+    st.dataframe(money_by_company)
+
+    st.title("Here are top 5 rocket companies-")
     helper.RocketMoney(df_)
+
